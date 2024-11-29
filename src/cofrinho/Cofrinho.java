aba7ba1514904db195aef2cad481da46aa970b16
Lucas <justinolucas@hotmail.com>
2024-11-29 10:29:27 -0300

    Terceiro Commit

--- src/cofrinho/Cofrinho.java
+++ src/cofrinho/Cofrinho.java
@@ -4,17 +4,39 @@ import java.util.ArrayList;
 
 public class Cofrinho {
 
-    ArrayList<Moeda> dindin = new ArrayList<Moeda>();
+    ArrayList<Moeda> dindin = new ArrayList<Moeda>(); // Lista de Array para o cofre
 
-    public void adicionar(Moeda d) {
+    public void adicionar(Moeda d) { // Metodo para adicionar moeda ao cofre
         dindin.add(d);
     }
 
-    public void remover(Moeda d) {
-        dindin.remove(d);
+    public void remover(String nomeTipo, double valor2) { // Metodo para remover moeda do cofre
+        Moeda moedaRemove = null;
+        final double EPSILON = 0.0001; // Margem de erro para comparação de doubles
+        for (Moeda moeda : dindin) {
+            if (moeda.info().equalsIgnoreCase(nomeTipo)) {  // Verifica o tipo da moeda com a do array
+                moedaRemove = moeda;
+                break;
+            }
+        }
+        if (moedaRemove != null) {  // Verifica se moeda foi achada no cofre
+            if (moedaRemove.valor >= valor2) {
+                moedaRemove.valor -= valor2; // Subtrai o valor
+                System.out.println("Removidos " + valor2 + " de " + nomeTipo + ".");
+                if (Math.abs(moedaRemove.valor) < EPSILON) {
+                    dindin.remove(moedaRemove); // Remove a moeda se o valor restante for zero
+                    System.out.println(nomeTipo + " removido do cofrinho (saldo zerado).");
+                }
+            } else {
+                System.out.println("Valor a remover maior que o disponível (" + moedaRemove.valor + ").");
+            }
+        } else {
+            System.out.println("Tipo de Moeda não encontrada.");
+        }
+
     }
 
-    public void listar(){
+    public void listar(){  // Metodo para Listar moedas no cofre
         if (dindin.isEmpty()) {
             System.out.println("Nenhuma moeda no cofre!");
         } else {
@@ -25,7 +47,7 @@ public class Cofrinho {
         }
     }
 
-    public double calcularConversao(){
+    public double calcularConversao(){  // Metodo para Converter moedas para real
         double total = 0;
         for (Moeda d : dindin){
             total += d.converterReal();
--- src/cofrinho/Main.java
+++ src/cofrinho/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void limpandoCsl() {
+    public static void limpandoCsl() { // Metodo para limpar console !! NAO FUNCIONA EM IDE !! Só funciona em CMD do Windows ou Linux
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
@@ -29,7 +29,7 @@ public class Main {
         System.out.println("#         0 - Encerrar Programa         #");
         System.out.println("#                                       #");
         System.out.println("<--------------------------------------->");
-        System.out.println("Opção Selecionada: " + (opcao = tecla.nextInt()));
+        System.out.println("Opção Selecionada: " + (opcao = tecla.nextInt()) + "\n");
 
 
         while (true) {
@@ -66,11 +66,33 @@ public class Main {
                     break;
                 case 3:
                     limpandoCsl();
+                    System.out.println("<--------------------------------------->");
+                    System.out.println("#           Remover Moedas              #");
+                    System.out.println("#                                       #");
+                    System.out.println("#         1 - Real                      #");
+                    System.out.println("#         2 - Dolar                     #");
+                    System.out.println("#         3 - Euro                      #");
+                    System.out.println("#                                       #");
+                    System.out.println("<--------------------------------------->");
+                    int tipo2 = tecla.nextInt();
+                    System.out.println("Digite a quantidade de valor da moeda para remover: ");
+                    double valor2 = tecla.nextDouble();
+                    String nomeTipo = null;
 
+                    if (tipo2 == 1) {
+                      nomeTipo = "Real";
+                    } else if (tipo2 == 2) {
+                        nomeTipo = "Dolar";
+                    } else if (tipo2 == 3) {
+                        nomeTipo = "Euro";
+                    } else {
+                        System.out.println("Tipo inválido.");
+                    }
+                    if(nomeTipo != null)cofrinho.remover(nomeTipo, valor2);
                     break;
                 case 4:
                     limpandoCsl();
-
+                    System.out.printf("Total de moedas convertido para Real: R$ %.2f%n", cofrinho.calcularConversao());
                     break;
                 case 0:
                     System.out.println("Saindo...");
